a163c11d23e4849c31e2db5f8098f5dc3bc5f1ac
Nikhil Bhasin <50350404+phoenix-1-2@users.noreply.github.com>
2021-03-02 15:12:23 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -292,54 +292,54 @@ var floatingLabel = function () {
 
 
 // scroll
-// var scrollWindow = function() {
-// 	var lastScrollTop = 0;
-// 	$(window).scroll(function(event){
-// 		var $w = $(this),
-// 				st = $w.scrollTop(),
-// 				navbar = $('.unslate_co--site-nav');
-// 				// sd = $('.js-scroll-wrap');
+var scrollWindow = function() {
+	var lastScrollTop = 0;
+	$(window).scroll(function(event){
+		var $w = $(this),
+				st = $w.scrollTop(),
+				navbar = $('.unslate_co--site-nav');
+				// sd = $('.js-scroll-wrap');
 
-// 		if (st > 150) {
-// 			if ( !navbar.hasClass('scrolled') ) {
-// 				navbar.addClass('scrolled');	
-// 			}
-// 		} 
-// 		if (st < 150) {
-// 			if ( navbar.hasClass('scrolled') ) {
-// 				navbar.removeClass('scrolled sleep');
-// 			}
-// 		} 
-// 		if ( st > 350 ) {
-// 			if ( !navbar.hasClass('awake') ) {
-// 				navbar.addClass('awake');	
-// 			} 
+		if (st > 150) {
+			if ( !navbar.hasClass('scrolled') ) {
+				navbar.addClass('scrolled');	
+			}
+		} 
+		if (st < 150) {
+			if ( navbar.hasClass('scrolled') ) {
+				navbar.removeClass('scrolled sleep');
+			}
+		} 
+		if ( st > 350 ) {
+			if ( !navbar.hasClass('awake') ) {
+				navbar.addClass('awake');	
+			} 
 
-// 			// hide / show on scroll
-// 			if (st > lastScrollTop){
-// 	      // downscroll code
-// 	      navbar.removeClass('awake');	
-// 	      navbar.addClass('sleep');	
-// 	   	} else {
-// 	      // upscroll code
-// 	      navbar.addClass('awake');	
-// 	   	}
-// 	   	lastScrollTop = st;
+			// hide / show on scroll
+			if (st > lastScrollTop){
+	      // downscroll code
+	      navbar.removeClass('awake');	
+	      navbar.addClass('sleep');	
+	   	} else {
+	      // upscroll code
+	      navbar.addClass('awake');	
+	   	}
+	   	lastScrollTop = st;
 			
 
-// 		}
-// 		if ( st < 350 ) {
-// 			if ( navbar.hasClass('awake') ) {
-// 				navbar.removeClass('awake');
-// 				navbar.addClass('sleep');
-// 			}
-// 		}
+		}
+		if ( st < 350 ) {
+			if ( navbar.hasClass('awake') ) {
+				navbar.removeClass('awake');
+				navbar.addClass('sleep');
+			}
+		}
 
    
 
-// 	});
+	});
 
-// };
+};
 
 
 var counter = function() {
