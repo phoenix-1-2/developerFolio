b6eb63988a433a2e1131fcc7c2ac08deb5025a7a
Nikhil Bhasin <50350404+phoenix-1-2@users.noreply.github.com>
2021-03-14 00:01:00 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -543,7 +543,7 @@ var contactForm = function() {
 				},
 				message: {
 					required: true,
-					minlength: 5
+					minlength: 1
 				}
 			},
 			messages: {
@@ -558,36 +558,36 @@ var contactForm = function() {
 				var $submit = $('.submitting'),
 					waitText = 'Submitting...';
 
-				$.ajax({   	
-			      type: "POST",
-			      url: "php/send-email.php",
-			      data: $(form).serialize(),
+			// 	$.ajax({   	
+			//       type: "POST",
+			//       url: "php/send-email.php",
+			//       data: $(form).serialize(),
 
-			      beforeSend: function() { 
-			      	$submit.css('display', 'block').text(waitText);
-			      },
-			      success: function(msg) {
-	               if (msg == 'OK') {
-	               	$('#form-message-warning').hide();
-			            setTimeout(function(){
-	               		$('#contactForm').fadeOut();
-	               	}, 1000);
-			            setTimeout(function(){
-			               $('#form-message-success').fadeIn();   
-	               	}, 1400);
+			//       beforeSend: function() { 
+			//       	$submit.css('display', 'block').text(waitText);
+			//       },
+			//       success: function(msg) {
+	        //        if (msg == 'OK') {
+	        //        	$('#form-message-warning').hide();
+			//             setTimeout(function(){
+	        //        		$('#contactForm').fadeOut();
+	        //        	}, 1000);
+			//             setTimeout(function(){
+			//                $('#form-message-success').fadeIn();   
+	        //        	}, 1400);
 		               
-		            } else {
-		               $('#form-message-warning').html(msg);
-			            $('#form-message-warning').fadeIn();
-			            $submit.css('display', 'none');
-		            }
-			      },
-			      error: function() {
-			      	$('#form-message-warning').html("Something went wrong. Please try again.");
-			         $('#form-message-warning').fadeIn();
-			         $submit.css('display', 'none');
-			      }
-		      });    		
+		    //         } else {
+		    //            $('#form-message-warning').html(msg);
+			//             $('#form-message-warning').fadeIn();
+			//             $submit.css('display', 'none');
+		    //         }
+			//       },
+			//       error: function() {
+			//       	$('#form-message-warning').html("Something went wrong. Please try again.");
+			//          $('#form-message-warning').fadeIn();
+			//          $submit.css('display', 'none');
+			//       }
+		    //   });    		
 	  		}
 			
 		} );
